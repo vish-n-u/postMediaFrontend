33cd7704ef81e91abe709562afb5f86a20566ccb
vish-n-u <vishnuna26@gmail.com>
2023-12-26 17:52:09 +0530

    url

--- src/url.js
+++ src/url.js
@@ -1,18 +1,18 @@
 export const loginUrl =
 "https://backend-postmedia-nonn.onrender.com/postMedia/api/v1/login";
 export const registerUrl =
-  "https://backend-postmedia-nonn.onrender.com/api/v1/register";
+  "https://backend-postmedia-nonn.onrender.com/postMedia/api/v1/register";
 export const createPostUrl  = 
-"https://backend-postmedia-nonn.onrender.com/api/v1/posts";
+"https://backend-postmedia-nonn.onrender.com/postMedia/api/v1/posts";
 export const getPostsUrl  =
-"https://backend-postmedia-nonn.onrender.com/api/v1/posts";
+"https://backend-postmedia-nonn.onrender.com/postMedia/api/v1/posts";
 
 export const createCommentsUrl  = 
-"https://backend-postmedia-nonn.onrender.com/api/v1/comments";
+"https://backend-postmedia-nonn.onrender.com/postMedia/api/v1/comments";
 
 
 export const getCommentsUrl  = 
-"https://backend-postmedia-nonn.onrender.com/api/v1/comment";
+"https://backend-postmedia-nonn.onrender.com/postMedia/api/v1/comment";
 
 export const getSearchResultsUrl =
-"https://backend-postmedia-nonn.onrender.com/api/v1/postsSearch/"
+"https://backend-postmedia-nonn.onrender.com/postMedia/api/v1/postsSearch/"
